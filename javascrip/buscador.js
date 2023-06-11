e6de9ddd01cf9d566848e9beae2a7f41c3df6111
Bernardo Martinez <jbernardom1@hotmail.com>
2023-06-10 21:26:22 -0500

    mi busqueda

--- javascrip/buscador.js
+++ javascrip/buscador.js
@@ -10,19 +10,19 @@ function realizarBusqueda(){
             titulo: 'Tubo figurado'
         },
         {
-            link: '',
+            link: 'https://www.mundialdetornillos.com/catalogo/racores',
             titulo: 'Racores'
         },
         {
-            link: '',
+            link: 'https://www.homecenter.com.co/homecenter-co/category/cat1690112/mangueras/',
             titulo: 'Manqueras'
         },
         {
-            link: '',
+            link: 'https://www.google.com/search?rlz=1C1CHZL_esCO1057CO1057&q=acoples+de+mangueras&tbm=isch&source=univ&fir=PAZi6P2TTivsVM%252CUd55cGI-fE433M%252C_%253BDTRCrZRe224BqM%252CI78DMooT_6tUNM%252C_%253Bzf6TsDzXvBXfjM%252CI78DMooT_6tUNM%252C_%253BW9WdcpJ5ZQQw2M%252CzA7FifH0cKmJ2M%252C_%253BQDZR2EXMj0baZM%252CrqLaJgM-MohEMM%252C_%253BSrl4iVQ8qsjNxM%252C3Tdw3_OX3zqARM%252C_%253BDnl6rdKk1YFy8M%252Cj3f5qBpihxLqrM%252C_%253B4WNa17POckB7OM%252CHUSboz_rzPF0_M%252C_%253BLS3elBtfXkK3pM%252CUd55cGI-fE433M%252C_%253ByzWIpJrDZYO90M%252C9iLHFT3LvhSz6M%252C_&usg=AI4_-kSxFv53nmI8R6l8IIbVrDtslBcUHQ&sa=X&ved=2ahUKEwi-19rYk7r_AhXxnGoFHfswB_EQjJkEegQIGRAC&biw=1366&bih=657&dpr=1',
             titulo: 'Acoples'
         },
         {
-            link: '',
+            link: 'https://www.c3carecarcenter.com/frenos-bogota/',
             titulo: 'Frenos'
         }
     ]
