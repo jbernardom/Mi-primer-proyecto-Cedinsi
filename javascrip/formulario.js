b10d6942a03450603f0f6f718edc604c1593c42f
Bernardo Martinez <jbernardom1@hotmail.com>
2023-06-22 23:14:51 -0500

    cambios formulario

--- javascrip/formulario.js
+++ javascrip/formulario.js
@@ -6,11 +6,34 @@ let timeout = null;
 document.querySelectorAll('.form-box').forEach((box) =>{
     const boxInput = box.querySelector('input');
 
-    boxInput.addEventListener('keydown', (Event) =>{
+    boxInput.addEventListener('keydown', (event) => {
         clearTimeout(timeout);
-        timeout = setTimeout(() =>{
+        timeout = setTimeout(() => {
             console.log(`input ${boxInput.name} value:`, boxInput.value);
+
+            validation(box, boxInput)
         }, 300);
         
     });
 });
+
+validation = (box, boxInput) => {
+    if(boxInput.name == 'nickName') {
+        console.log('validacion para nombre');
+        if (boxInput.value == '') {
+            showError(true, box);
+        }else{
+           showError(false, box);  
+        }
+    }
+};
+
+showError = (check, box)=> {
+    if(check) {
+        box.classList.remove('form-success');
+        box.classList.add('form-error');
+    }else{
+        box.classList.remove('form-error');
+        box.classList.add('form-success');  
+    }
+};
