1a17e7dd3594afcb9868dd9ceda7b3d6fb34b4f3
Bernardo Martinez <jbernardom1@hotmail.com>
2023-06-23 21:48:38 -0500

    cambiosformulario

--- javascrip/formulario.js
+++ javascrip/formulario.js
@@ -1,39 +1,13 @@
-const form = document.getElementById('user-form');
-const submitButton = document.getElementById('submit-btn');
 
-let timeout = null;
 
-document.querySelectorAll('.form-box').forEach((box) =>{
-    const boxInput = box.querySelector('input');
+const firstName = document.querySelector('#firstNameInput');
+const lastName = document.querySelector('#lastNameInput');
+const tel = document.querySelector('#telInput');
+const email = document.querySelector('#emailInput');
 
-    boxInput.addEventListener('keydown', (event) => {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            console.log(`input ${boxInput.name} value:`, boxInput.value);
+const firstNameError = document.querySelector('#firstNameError');
+const lastNameError = document.querySelector('#lastNameError');
+const telError = document.querySelector('#telError');
+const emailError = document.querySelector('#emailError');
 
-            validation(box, boxInput)
-        }, 300);
-        
-    });
-});
 
-validation = (box, boxInput) => {
-    if(boxInput.name == 'nickName') {
-        console.log('validacion para nombre');
-        if (boxInput.value == '') {
-            showError(true, box);
-        }else{
-           showError(false, box);  
-        }
-    }
-};
-
-showError = (check, box)=> {
-    if(check) {
-        box.classList.remove('form-success');
-        box.classList.add('form-error');
-    }else{
-        box.classList.remove('form-error');
-        box.classList.add('form-success');  
-    }
-};
