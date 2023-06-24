9f931b02502c86b11f93a5e5c15b95f054d2f043
Bernardo Martinez <jbernardom1@hotmail.com>
2023-06-23 23:26:15 -0500

    prueva

--- javascrip/formulario.js
+++ javascrip/formulario.js
@@ -10,4 +10,29 @@ const lastNameError = document.querySelector('#lastNameError');
 const telError = document.querySelector('#telError');
 const emailError = document.querySelector('#emailError');
 
+const button = document.querySelector('#submit');
+
+button.addEventListener('click', (event)=>{
+    event.preventDefault();
+    validateEmpty(firstName.value, firstName, firstNameError);
+});
+
+function validateEmpty(valueInput, divInput, divError){
+    if(valueInput.length == 0){
+         showError(divInput, divError);
+    }else{
+        hideError(divInput, divError);
+    }
+}
+
+function showError(divInput, divError){
+     divInput.style.border = '1px solid red';
+     divError.innerHTML = `<img class="icon-error" src="./Imagenes/icon-error.svg" alt="">
+     <p class="error">Introduce un nombre valido</p>`;
+}
+
+function hideError(divInput){
+    divInput.style.border = '1px solid #1e9ba3';
+}
+
 
